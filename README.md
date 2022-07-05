af16b10078d71a0d0abb808e91e81967438c2c75
chaimakr <chaima050@gmail.com>
2022-07-05 12:46:26 +0100

    testing Koa initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## KoaJS introduction
+Modern and minimalist Node.js Framework
+<br>
+This project is inspired by traversy media <a link="https://www.youtube.com/watch?v=z84uTk5zmak " >tutorial</a> .
+### specifications
+* uses context obj ( contains req & res ) , refer to as ctx usually
+* 
---
+++ app.js
@@ -0,0 +1,54 @@
+const Koa = require('koa');
+const KoaRouter = require ('koa-router');
+const json = require('koa-json');
+const path = require('path');
+const render = require('koa-ejs');
+
+
+const app = new Koa();
+const router = new KoaRouter(); 
+
+// local data ( instead of db )
+const things = ["get familliar with Hive tech requirements",
+   " analyse the existing codebase of Hive Project ",
+   " don't forget about the Onboarding checklist "   
+]
+
+//json prettier middleware
+app.use(json());
+
+// starter example
+//app.use( async ctx => ctx.body = 'hello world');
+
+render(app, {
+    root :  path.join(__dirname, 'views'),
+    layout : 'layout',
+    viewExt : 'html',
+    cache : false,
+    debug : false
+})
+
+// index routers
+router.get('/', index);
+router.get('/add', showAdd);
+
+
+// display list of things
+async function index(ctx ){
+    await ctx.render('index', {
+        title : 'Things I need to do this week',
+        things : things
+    });
+}
+// show add page
+async function showAdd(ctx ){
+    await ctx.render('add');
+}
+
+
+router.get('/test', (ctx => ctx.body = "hello test"))
+
+//Router Middleware 
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(3000, () => console.log('server started..'))
